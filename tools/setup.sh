f5b256035cf5a214edb0d2e2a10cd37c0d6e4ef5
Yota Toyama <raviqqe@gmail.com>
2025-01-07 19:22:07 +0900

    Fix library paths

--- tools/setup.sh
+++ tools/setup.sh
@@ -11,5 +11,5 @@ llvm_prefix=$(brew --prefix llvm@$llvm_version)
 
 echo TABLEGEN_190_PREFIX=$llvm_prefix >>$GITHUB_ENV
 echo PATH=$llvm_prefix/bin:$PATH >>$GITHUB_ENV
-echo LIBRARY_PATH=$(brew --prefix)/lib:$LIBRARY_PATH >>$GITHUB_ENV
-echo LD_LIBRARY_PATH=$(brew --prefix)/lib:$LD_LIBRARY_PATH >>$GITHUB_ENV
+echo LIBRARY_PATH=$llvm_prefix/lib:$LIBRARY_PATH >>$GITHUB_ENV
+echo LD_LIBRARY_PATH=$llvm_prefix/lib:$LD_LIBRARY_PATH >>$GITHUB_ENV
