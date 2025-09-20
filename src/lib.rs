80528d7635d2d634568a2ae0fce86fdd18125775
Yota Toyama <raviqqe@gmail.com>
2025-09-19 20:48:49 -0700

    Fix

--- src/lib.rs
+++ src/lib.rs
@@ -212,7 +212,7 @@ impl<'s> TableGenParser<'s> {
         }
     }
 
-    pub fn source_info(&self) -> SourceInfo {
+    pub fn source_info(&self) -> SourceInfo<'_> {
         SourceInfo(self)
     }
 
