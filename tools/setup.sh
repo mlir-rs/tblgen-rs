5b4dc0e610f3bfcea70f5ef014e30466779fb030
Edgar Luque <git@edgl.dev>
2026-03-20 09:47:35 +0100

    Update CI to LLVM 22, add LLVM 21 test matrix

--- tools/setup.sh
+++ tools/setup.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-llvm_version=21
+llvm_version=${LLVM_VERSION:-22}
 
 brew update
 brew install llvm@$llvm_version
 
 llvm_prefix=$(brew --prefix llvm@$llvm_version)
 
-echo TABLEGEN_210_PREFIX=$llvm_prefix >>$GITHUB_ENV
+echo TABLEGEN_${llvm_version}0_PREFIX=$llvm_prefix >>$GITHUB_ENV
 echo PATH=$llvm_prefix/bin:$PATH >>$GITHUB_ENV
 echo LIBRARY_PATH=$llvm_prefix/lib:$LIBRARY_PATH >>$GITHUB_ENV
 echo LD_LIBRARY_PATH=$llvm_prefix/lib:$LD_LIBRARY_PATH >>$GITHUB_ENV
